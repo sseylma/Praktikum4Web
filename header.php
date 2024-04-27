f820f7e095be50cf8dad79a14d831f3ff782f6e4
sseylma <sseylmao@gmail.com>
2024-04-28 02:34:53 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <title>Contoh Modularisasi</title> 
+    <link href="style.css" rel="stylesheet" type="text/stylesheet" 
+    media="screen" /> 
+</head> 
+<body> 
+    <div class="container"> 
+    <header> 
+        <h1>Modularisasi Menggunakan Require</h1> 
+    </header> 
+    <nav> 
+        <a href="home">Home</a> 
+        <a href="about">Tentang</a> 
+        <a href="kontak">Kontak</a> 
+    </nav> 
